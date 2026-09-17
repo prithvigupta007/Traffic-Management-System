e2d9b8be6e41f7914cba0f066cbbf52b244409fa
Prithvi Gupta <prithviiit2007@gmail.com>
2026-09-17 21:27:39 +0530

    update clock_timer.v

---
+++ clock_timer.v
@@ -0,0 +1,52 @@
+module clock_timer #(parameter integer CLK_FREQ=50000000)(
+    input wire clk,
+    input wire rst,
+    input wire [5:0]target_seconds,
+    input wire phase_start,
+    output reg one_sec_tick,
+    output reg [5:0]remaining_time,
+    output reg phase_done
+);
+    reg [31:0]clk_count;
+    always @(posedge clk)
+    begin
+        if(rst)
+	      begin
+	          clk_count <= 0;
+            one_sec_tick <= 0;
+            remaining_time <= 0;
+            phase_done <= 0;
+	      end
+	      else
+	      begin
+	          if(clk_count == CLK_FREQ-1)
+	          begin
+		            clk_count <= 0;
+		            one_sec_tick <= 1;
+	          end
+	          else
+	          begin
+		            clk_count <= clk_count+1;
+		            one_sec_tick <= 0;
+	          end
+
+	          if(phase_start)
+	          begin
+		            remaining_time <= target_seconds;
+                phase_done <= 0;
+	          end
+	          else if(one_sec_tick) 
+	          begin
+                if(remaining_time>1) 
+		            begin
+                    remaining_time <= remaining_time-1;
+                end
+                else 
+		            begin
+                    remaining_time <= 0;
+                    phase_done <= 1;
+                end
+	          end
+	      end
+    end
+endmodule
