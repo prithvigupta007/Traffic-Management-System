749d80f4364fd47f0cf45f004cdd4e9202f3f93e
Satyam Kumar <skjha9801206516@gmail.com>
2026-09-19 01:36:28 +0530

    Refactor adaptive_controller for four-way traffic control
    Updated the adaptive_controller module to handle traffic counts from four directions instead of two, and adjusted the logic for determining green time based on the new traffic density calculations.

--- adaptive-controller.v
+++ adaptive-controller.v
@@ -1,23 +1,34 @@
-module adaptive_controller(input wire[2:0]traffic_a_count , input wire[2:0]traffic_b_count,
-                           output reg[5:0]green_time_a, output reg[5:0]green_time_b);
+module adaptive_controller(input wire[2:0]traffic_n_count ,input wire[2:0]traffic_s_count , input wire[2:0]traffic_e_count,
+                           input wire[2:0]traffic_w_count, output reg[5:0]green_time_ns, output reg[5:0]green_time_ew);
 
-  // giving green time on the basis of traffic density
+  reg [3:0] ns_demand;
+  reg [3:0] ew_demand;
+
+  
   
   always @(*) begin
-    if(traffic_a_count<=1)
-      green_time_a=10;
-    else if (traffic_a_count<=3)
-      green_time_a=15;
-    else
-      green_time_a=20;
 
+    // calculate total traffic demand 
+    ns_demand = traffic_n_count + traffic_s_count;
+    ew_demand = traffic_e_count + traffic_w_count;
+    
+    // giving green time on the basis traffic density 
 
-    if(traffic_b_count<=1)
-      green_time_b=10;
-    else if(traffic_b_count<=3)
-      green_time_b=15;
+    // for ns
+    if(ns_demand <=1)
+      green_time_ns=10;
+    else if (ns_demand<=3)
+      green_time_ns=15;
     else
-      green_time_b=20;
+      green_time_ns=20;
+
+  // for ew
+    if(ew_demand<=1)
+      green_time_ew=10;
+    else if(ew_demand<=3)
+      green_time_ew=15;
+    else
+      green_time_ew=20;
 
   end
   endmodule
